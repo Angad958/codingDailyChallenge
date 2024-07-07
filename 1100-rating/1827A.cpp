d1ee455cf510750c59f1ffbbebb5d4ef4d1ab2a2
Angad958 <81446869+Angad958@users.noreply.github.com>
2024-07-08 00:15:39 +0530

    day-8

---
+++ 1100-rating/1827A.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <cstring>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define Fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vl v1, v2;
+        fo(i,n)
+        {
+            ll a;
+            cin >> a;
+            v1.pb(a);
+        }
+        fo(i,n)
+        {
+            ll a;
+            cin >> a;
+            v2.pb(a);
+        }
+        sortall(v1);
+        sortall(v2);
+        int c = 0;
+        ll ans = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            ll ind = upper_bound(v1.begin(), v1.end(), v2[i]) - v1.begin();
+            ind = n - ind -c;
+            c++;
+            ans = (ans * ind)%mod;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
