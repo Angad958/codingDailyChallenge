3591ed6ab1a1a4818d098a8a3af897b41c39ff91
Angad958 <81446869+Angad958@users.noreply.github.com>
2024-07-13 00:47:13 +0530

    day-10

---
+++ 1100-rating/1899C.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <cstring>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define Fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+vl findDivisor(ll a)
+{
+    vl ans;
+    for (ll i = 1; i <= sqrt(a); i++)
+    {
+        if (a % i == 0)
+        {
+            ans.pb(i);
+            if (i != a / i)
+            {
+                ans.pb(a / i);
+            }
+        }
+    }
+    return ans;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vl v(n);
+        fo(i, n) cin >> v[i];
+        int i = 0;
+        while (v[i] < 0)
+        {
+            i++;
+        }
+        ll ans = INT_MIN , sum=0;
+        for (int j = 1; j < n; j++)
+        {
+         
+            ans = max(ans, v[j]);
+            sum += v[j];
+            ans = max(ans, sum);
+            if(sum<0)
+                sum = 0;
+            if ((abs(v[j]) % 2 ==abs(v[j - 1]) % 2 ) )
+            {
+                sum = 0;
+                continue;
+            }
+        }
+       
+        sort(v.begin(), v.end());
+        if(v[n-1]<0)
+        {
+            cout << v[n - 1] << endl;
+            continue;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
---
+++ 1100-rating/1971D.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <cstring>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define Fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+vl findDivisor(ll a)
+{
+    vl ans;
+    for (ll i = 1; i <= sqrt(a); i++)
+    {
+        if (a % i == 0)
+        {
+            ans.pb(i);
+            if (i != a / i)
+            {
+                ans.pb(a / i);
+            }
+        }
+    }
+    return ans;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        vpl v;
+        int ans = 1,flag=0;
+        for (int i = 0; i < s.size() - 1; i++)
+        {
+            if(s[i]!=s[i+1])
+            {
+                ans++;
+            }
+            if(s[i]=='0' && s[i+1]=='1')
+                flag = 1;
+        }
+        cout << ans - flag << endl;
+    }
+    return 0;
+}
